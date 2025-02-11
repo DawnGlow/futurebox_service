a8226b92e71e0058dac54ee38e0770b7ff373b57
DawnGlow <ferrater1013@gmail.com>
2025-02-12 01:29:58 +0900

    chore(-): spring boot initializing

---
+++ src/main/java/com/future_package/futurebox_service/FutureboxServiceApplication.java
@@ -0,0 +1,13 @@
+package com.future_package.futurebox_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureboxServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureboxServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/future_package/futurebox_service/FutureboxServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.future_package.futurebox_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureboxServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
